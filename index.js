ff593054b9d07a8465a9f66e0ae6a63f13883be7
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2024-05-30 09:43:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 1337
 
 const cors = require('cors')
 app.use(cors())
